1548a814cae8851318fd116a8583962f20049d70
shinbunbun <takamuneyuto@gmail.com>
2024-05-06 03:50:30 +0900

    impl pivot_root

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 extern crate alloc;
-use std::{env, fs::write, io::Error, path::Path};
+use std::{
+    env,
+    fs::{create_dir_all, remove_dir_all, set_permissions, write, Permissions},
+    io::Error,
+    os::unix::fs::PermissionsExt,
+    path::Path,
+};
 
 use alloc::ffi::CString;
 use nix::{
     libc::{getgid, getuid, CLONE_NEWUTS, SIGCHLD},
-    mount::{mount, MsFlags},
+    mount::{mount, umount, umount2, MntFlags, MsFlags},
     sched::{clone, CloneFlags},
     sys::wait::waitpid,
-    unistd::{execv, execve, sethostname},
+    unistd::{chdir, chroot, execv, execve, pivot_root, sethostname},
 };
+
+const HOME_DIR: &str = "/home/bunbun";
 
 fn child_fn() -> isize {
     println!("staet child process");
@@ -67,10 +75,10 @@ fn init_container() {
         Err(e) => {
             println!("Set hostname failed: {}", e);
         }
-    }
+    };
     match mount(
         Some("proc"),
-        "/proc",
+        format!("{}/root/rootfs/proc", HOME_DIR).as_str(),
         Some("proc"),
         MsFlags::MS_NOEXEC | MsFlags::MS_NOSUID | MsFlags::MS_NODEV,
         None::<&str>,
@@ -82,6 +90,89 @@ fn init_container() {
             println!("Mount proc failed: {}", e);
         }
     };
+    /* match chroot(format!("{}/root/rootfs", HOME_DIR).as_str()) {
+        Ok(_) => {
+            println!("Chroot success");
+        }
+        Err(e) => {
+            println!("Chroot failed: {}", e);
+        }
+    }; */
+    match chdir(format!("{}/root", HOME_DIR).as_str()) {
+        Ok(_) => {
+            println!("Change dir success");
+        }
+        Err(e) => {
+            println!("Change dir failed: {}", e);
+        }
+    };
+    match mount(
+        Some("rootfs"),
+        format!("{}/root/rootfs", HOME_DIR).as_str(),
+        None::<&str>,
+        MsFlags::MS_BIND | MsFlags::MS_REC,
+        None::<&str>,
+    ) {
+        Ok(_) => {
+            println!("Mount rootfs success");
+        }
+        Err(e) => {
+            println!("Mount rootfs failed: {}", e);
+        }
+    };
+    match create_dir_all(format!("{}/root/rootfs/oldrootfs", HOME_DIR)) {
+        Ok(_) => {
+            println!("Create oldrootfs success");
+        }
+        Err(e) => {
+            println!("Create oldrootfs failed: {}", e);
+        }
+    };
+    let perm = Permissions::from_mode(0o700);
+    match set_permissions(format!("{}/root/rootfs/oldrootfs", HOME_DIR), perm) {
+        Ok(_) => {
+            println!("Set permission success");
+        }
+        Err(e) => {
+            println!("Set permission failed: {}", e);
+        }
+    };
+    match pivot_root(
+        format!("{}/root/rootfs", HOME_DIR).as_str(),
+        format!("{}/root/rootfs/oldrootfs", HOME_DIR).as_str(),
+    ) {
+        Ok(_) => {
+            println!("Pivot root success");
+        }
+        Err(e) => {
+            println!("Pivot root failed: {}", e);
+        }
+    };
+    match umount2("/oldrootfs", MntFlags::MNT_DETACH) {
+        Ok(_) => {
+            println!("Umount oldrootfs success");
+        }
+        Err(e) => {
+            println!("Umount oldrootfs failed: {}", e);
+        }
+    };
+    match remove_dir_all("/oldrootfs") {
+        Ok(_) => {
+            println!("Remove oldrootfs success");
+        }
+        Err(e) => {
+            println!("Remove oldrootfs failed: {}", e);
+        }
+    };
+    match chdir("/") {
+        Ok(_) => {
+            println!("Change dir success");
+        }
+        Err(e) => {
+            println!("Change dir failed: {}", e);
+        }
+    };
+
     let c = CString::new("/bin/sh").unwrap();
     let argv = [CString::new("/bin/sh").unwrap()];
     let envp: Vec<CString> = env::vars()
