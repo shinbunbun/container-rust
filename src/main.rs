8b367d9edaefb06824ad2d27e651ab0e2e30dd56
shinbunbun <takamuneyuto@gmail.com>
2024-05-05 16:13:08 +0900

    impl clone

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use nix::sched::{clone, CloneFlags};
+
+fn child_fn() -> isize {
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let flags = CloneFlags
+    let child = Box::new(child_fn);
+    let child_stack = &mut [0; 1024];
+    let flags = CloneFlags::CLONE_NEWIPC | CloneFlags::CLONE_NEWNET | CloneFlags::CLONE_NEWUSER;
+    let pid = unsafe { clone(child, child_stack, flags, None) };
+    match pid {
+        Ok(pid) => {
+            println!("Child pid: {}", pid);
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
 }
