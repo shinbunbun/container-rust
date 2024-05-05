604f5151b9b12bcb195984b72ad78aa6c8226665
shinbunbun <takamuneyuto@gmail.com>
2024-05-05 16:52:31 +0900

    exec shell

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
-use nix::sched::{clone, CloneFlags};
+extern crate alloc;
+use alloc::ffi::CString;
+use nix::{
+    libc::SIGCHLD,
+    sched::{clone, CloneFlags},
+    sys::wait::waitpid,
+    unistd::execv,
+};
 
 fn child_fn() -> isize {
-    0
-}
-
-fn main() {
-    // let flags = CloneFlags
-    let child = Box::new(child_fn);
-    let child_stack = &mut [0; 1024];
-    let flags = CloneFlags::CLONE_NEWIPC | CloneFlags::CLONE_NEWNET | CloneFlags::CLONE_NEWUSER;
-    let pid = unsafe { clone(child, child_stack, flags, None) };
-    match pid {
-        Ok(pid) => {
-            println!("Child pid: {}", pid);
+    println!("staet child process");
+    let c = CString::new("/bin/sh").unwrap();
+    let argv = [CString::new("sh").unwrap()];
+    match execv(&c, &argv) {
+        Ok(i) => {
+            println!("Success: {}", i);
         }
         Err(e) => {
             println!("Error: {}", e);
         }
+    };
+    0
+}
+
+fn main() {
+    println!("Parent pid: {}", std::process::id());
+
+    let cb = Box::new(child_fn);
+    let child_stack = &mut [0; 1024];
+    let flags = CloneFlags::CLONE_NEWIPC | CloneFlags::CLONE_NEWNET | CloneFlags::CLONE_NEWUSER;
+    let pid = match unsafe { clone(cb, child_stack, flags, Some(SIGCHLD)) } {
+        Ok(pid) => pid,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
+    println!("Child pid: {}", pid);
+    while let Ok(states) = waitpid(pid, None) {
+        println!("Exit status: {:?}", states);
     }
+    println!("Parent process exit")
 }
